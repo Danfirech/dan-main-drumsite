60e740214d79f101d61cd02f233de5a36a348697
Dan Possehl <danielbpossehl@gmail.com>
2022-02-04 20:08:55 -0800

    started component

---
+++ danosdrumpage/pages/components/Mainbackground.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const topcontainer = styled.div`
+  width: 100vw;
+  height: 50%;
+  background-color: black;
+`;
+
+const Mainbackground = () => {};
+
+export default Mainbackground;
